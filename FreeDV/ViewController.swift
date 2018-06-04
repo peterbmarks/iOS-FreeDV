07e29d373af066ee746a8ba445987dc6acaa8092
Peter B Marks <peterbmarks@gmail.com>
2018-06-04 11:37:24 +1000

    Wire up the buttons.

--- FreeDV/ViewController.swift
+++ FreeDV/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var toRadioButton: UIButton!
+  @IBOutlet weak var fromRadioButton: UIButton!
+  @IBOutlet weak var transmitSwitch: UISwitch!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+  @IBAction func onToRadioButton(_ sender: UIButton) {
+    print("Tapped the To radio audio button")
+  }
+  
+  @IBAction func onFromRadioButton(_ sender: UIButton) {
+    print("Tapped the From radio audio button")
+  }
+  
+  @IBAction func transmitSwitchAction(_ sender: UISwitch) {
+    print("Transmit switch changed")
+  }
+  
 }
 
