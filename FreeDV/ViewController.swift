781cce5cca6b7abca1945a3467a0b0a71b9297a8
Peter B Marks <peterbmarks@gmail.com>
2018-06-04 12:56:28 +1000

    Requests access to microphone.

--- FreeDV/ViewController.swift
+++ FreeDV/ViewController.swift
@@ -5,18 +5,25 @@
 //  Created by Peter Marks on 3/6/18.
 //  Copyright © 2018 Peter Marks. All rights reserved.
 //
+// Thanks: https://www.hackingwithswift.com/example-code/media/how-to-record-audio-using-avaudiorecorder
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
   @IBOutlet weak var toRadioButton: UIButton!
   @IBOutlet weak var fromRadioButton: UIButton!
   @IBOutlet weak var transmitSwitch: UISwitch!
+  @IBOutlet weak var startSwitch: UISwitch!
+  @IBOutlet weak var statusLabel: UILabel!
+  
+  var recordingSession: AVAudioSession!
   
   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      recordingSession = AVAudioSession.sharedInstance()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +31,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+  @IBAction func onStartSwitchChanged(_ sender: UISwitch) {
+    print("Start switch changed")
+    if sender.isOn == true {
+      print("switch now on")
+      do {
+        try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try recordingSession.setActive(true)
+        recordingSession.requestRecordPermission() { [unowned self] allowed in
+          DispatchQueue.main.async {
+            if allowed {
+              //self.loadRecordingUI()
+            } else {
+              // failed to record!
+            }
+          }
+        }
+      } catch {
+        // failed to record!
+        print("Error starting record")
+        statusLabel.text = "Error starting record session"
+      }
+    } else {
+      print("recording stopped")
+    }
+  }
+  
   @IBAction func onToRadioButton(_ sender: UIButton) {
     print("Tapped the To radio audio button")
   }
